cc7773f6678870399a6fd0434e56a1d559a33fde
LivingJW30 <118578583+LivingJW30@users.noreply.github.com>
2024-02-25 15:59:28 -0500

    Add all C source code

---
+++ bee.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "functions.h"
+
+void buzzin() {
+    printf("     _\n");
+    printf("    /_/      .'''.\n");
+    printf(" =O(_)))) ...'     `.\n");
+    printf("    \\_\\              `..'\n");
+    printf("\n");
+}
